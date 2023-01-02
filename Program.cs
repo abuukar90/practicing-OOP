a1b644e6cceea38f60ffc50ff0974e7a808ad6f3
abuukar <abuukar229605@gmail.com>
2023-01-01 19:03:01 -0500

    hey

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using practice_course;
+using System.Net.Mail;
+
+soccer mesurement = new soccer();
+mesurement.Length = 23;
+mesurement.DisplayInfo();
+
+
+soccer mesurement2 = new soccer();
+mesurement2.DisplayInfo();
+
+Console.ReadLine();
---
+++ soccer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_course
+{
+    public class soccer
+    {
+        public int Length { get; set; }
+        public int height;
+        public int width;
+        public int volume;
+
+        public soccer()
+        {
+            Length = 45;
+            this.height = 6;
+            this.width = 8;
+
+
+        }
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine(" length is {0} and hight is {1} and the width is {2} so the volume is {3}  " 
+                ,Length , height , width, volume = Length*height*width);
+        }
+
+
+       
+
+    }
+}
